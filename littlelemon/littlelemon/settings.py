e56c3cb1d6ddeb64c041f4d665097791e46aa54b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-29 16:51:37 +0300

    initial commit

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 
 # Application definition
